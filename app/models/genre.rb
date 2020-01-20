25adc46b146fdebdbf952af4ce96626aacb4c9ca
Michael Carroll <carrollm2@gmail.com>
2020-01-20 03:38:45 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,18 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artist.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.all.map do |artist|
+      artist_names << artist.name
+    end
   end
 end
