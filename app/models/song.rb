e1615f8e02db4f30b16e8d4ba3fe703803af30ca
Michael Carroll <carrollm2@gmail.com>
2020-01-20 04:42:18 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -23,7 +23,7 @@ class Song < ActiveRecord::Base
       drake_instance = Artist.create(name: "Drake")
     end
 
-    Song.create(artist: drake_instance )
+    Song.create(artist: drake_instance)
 
   end
 end
